a12631e3a4aedb88371352a3cf45609d91afeb46
MargaritaShum <lalayt666@gmail.com>
2016-10-05 10:12:59 +0300

    first commit

---
+++ client/app/Rita/rita.js
@@ -0,0 +1,18 @@
+(function() {
+varmoduleId = "Summ";
+angular.module("comeApp").controller(moduleId, [Summ]);
+
+functionSumm() {
+varsm = this;
+sm.items = [];
+
+init();
+
+functioninit() {
+            sm.items = [	{kkal:255}, 
+{kkal:333},{kkal:25}];
+        }
+    }
+	
+})(); 
+
